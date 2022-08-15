670f16effc13ba438437a109ca935932a31d8f8a
hlngoben021 <hlngoben021@student.wethinkcode.co.za>
2022-08-15 16:31:50 +0200

    update on decimal and binary

--- src/dec_hexa.java
+++ src/dec_hexa.java
@@ -26,11 +26,23 @@ class decimal_Hexadecimal {
         String hexadecimal_Number = Integer.toHexString(decimal_Number);
         System.out.println("Decimal number of :" + decimal_Number +" is = "+ hexadecimal_Number.toUpperCase() +" in Hexadecimal");
     }
+    int convert_Decimal_To_Binary(){
+        String binary_Number = Integer.toBinaryString(decimal_Number);
+        System.out.println("Decimal number of :" + decimal_Number +" is = "+ binary_Number +" in Binaries");
+        return Integer.parseInt((binary_Number));
+
+    }
+    void convert_Binary_To_Decimal(int decimal_Num){
+
+        System.out.println("Binary number of :" +  decimal_Num +" is = "+ decimal_Number +" in Decimal");
+    }
 }
 public class dec_hexa{
     public static void main(String arg[]){
         decimal_Hexadecimal obj =new decimal_Hexadecimal();
         obj.Decimal_Num();
         obj.convert_Decimal_To_Haxedecimal();
+        int binary_ = obj.convert_Decimal_To_Binary();
+        obj.convert_Binary_To_Decimal(binary_);
     }
 }
