f85cdca46b21b146c7020079469a6fd9bed319e4
hlngoben021 <hlngoben021@student.wethinkcode.co.za>
2022-08-16 08:56:47 +0200

    number between 2 and 10

--- src/dec_hexa.java
+++ src/dec_hexa.java
@@ -7,9 +7,11 @@ class decimal_Hexadecimal {
 
     int Decimal_Num(){
 
+        // the method which allows the user to give an input of a decimal number
+
         try {
             scanner = new Scanner(System.in);
-            System.out.print("Enter number in Decimal from 0 to 10000: ");
+            System.out.println("Enter number in Decimal from 0 to 10000: ");
             decimal_Number= scanner.nextInt();
             if (decimal_Number <=10000){
                 return decimal_Number;
@@ -22,29 +24,39 @@ class decimal_Hexadecimal {
 
 
     }
-    void convert_Decimal_To_Haxedecimal(){
+    int convert_Decimal_To_Haxedecimal(){
+
+        // converting decimal numbers to hexadecimal
         String hexadecimal_Number = Integer.toHexString(decimal_Number);
         System.out.println("Decimal number of :" + decimal_Number +" is = "+ hexadecimal_Number.toUpperCase() +" in Hexadecimal");
+        return decimal_Number;
     }
     int convert_Decimal_To_Binary(){
+        // converting decimal numbers to binaries
         String binary_Number = Integer.toBinaryString(decimal_Number);
         System.out.println("Decimal number of :" + decimal_Number +" is = "+ binary_Number +" in Binaries");
         return Integer.parseInt((binary_Number));
 
     }
     void convert_Binary_To_Decimal(int decimal_Num){
+        //converting binaries to decimal
 
         System.out.println("Binary number of :" +  decimal_Num +" is = "+ decimal_Number +" in Decimal");
     }
 }
 public class dec_hexa{
     public static void main(String arg[]){
+        //creating an object to call my method with my class
         decimal_Hexadecimal obj =new decimal_Hexadecimal();
-        obj.Decimal_Num();
+        int num_btw_2_N_10 = obj.Decimal_Num();
         obj.convert_Decimal_To_Haxedecimal();
-        int binary_ = obj.convert_Decimal_To_Binary();
-        if (binary_ >=2 && binary_<11){
-            obj.convert_Binary_To_Decimal(binary_);
+
+        //checking if the number is between 2 and 10 to convert to binaries and decimal
+        if (num_btw_2_N_10 >1 && num_btw_2_N_10 <=10){
+            int binary_decimal = obj.convert_Decimal_To_Binary();
+            obj.convert_Binary_To_Decimal(binary_decimal);
+
         }
+
     }
 }
